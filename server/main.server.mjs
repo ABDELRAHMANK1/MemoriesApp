915d48a31bb1b9a8cb9e3f7afc53274be49f6ac8
abdelrahman <abdelrahmank12a@gmail.com>
2025-09-06 02:19:49 +0300

    Fix email @ symbols in contact page

--- server/main.server.mjs
+++ server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-JIDHYSE3.mjs";import"./chunk-6TCRYDLQ.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-BQMXA2RU.mjs";import"./chunk-6TCRYDLQ.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
